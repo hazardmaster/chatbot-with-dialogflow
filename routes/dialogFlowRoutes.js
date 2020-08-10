a48fba102df05daa74a37b84e48d41e2de9732af
Oscar Hazard <oscarkipkoech@gmail.com>
2020-08-10 23:13:53 +0300

    connect server side and dialog flow

---
+++ routes/dialogFlowRoutes.js
@@ -0,0 +1,34 @@
+const chatbot = require('../chatbot/chatbot');
+
+module.exports = (app) => {
+  app.get('/', (req, res) => {
+    res.send({ hello: 'Johnny' });
+  });
+
+  app.post('/api/df_text_query', async (req, res) => {
+    const responses = await chatbot.textQuery(
+      req.body.text,
+      req.body.parameters
+    );
+
+    const result = responses[0].queryResult;
+    console.log(`Query: ${result.queryText}`);
+    console.log(`Response: ${result.fulfillmentText}`);
+    if (result.intent) {
+      console.log(`Intent: ${result.intent.displayName}`);
+    } else {
+      console.log(`No intent matched.`);
+    }
+
+    res.send(result);
+  });
+
+  app.post('/api/df_event_query', async (req, res) => {
+    const responses = await chatbot.eventQuery(
+      req.body.event,
+      req.body.parameters
+    );
+    const result = responses[0].queryResult;
+    res.send(result);
+  });
+};
--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+var bodyParser = require('body-parser');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hello: 'there' });
-});
+// configure the app to use bodyParser()
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+require('./routes/dialogFlowRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
