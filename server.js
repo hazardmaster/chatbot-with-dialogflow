a68a7a04884797d929d6adde26ca0460286ff4a4
Oscar Hazard <oscarkipkoech@gmail.com>
2020-08-16 13:28:08 +0300

    further changes

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ app.use(bodyParser.json());
 
 require('./routes/dialogFlowRoutes')(app);
 
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
