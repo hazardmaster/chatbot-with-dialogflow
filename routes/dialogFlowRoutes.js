1c944797ddbf320c6a952a407ac42dbd79476354
Oscar Hazard <oscarkipkoech@gmail.com>
2020-08-16 14:40:04 +0300

    further changes

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -2,21 +2,22 @@ const chatbot = require('../chatbot/chatbot');
 
 module.exports = (app) => {
   app.post('/api/df_text_query', async (req, res) => {
-    const responses = await chatbot.textQuery(
-      req.body.text,
-      req.body.parameters
-    );
+    res.send({ hello: 'Johnny' });
+    // const responses = await chatbot.textQuery(
+    //   req.body.text,
+    //   req.body.parameters
+    // );
 
-    const result = responses[0].queryResult;
-    console.log(`Query: ${result.queryText}`);
-    console.log(`Response: ${result.fulfillmentText}`);
-    if (result.intent) {
-      console.log(`Intent: ${result.intent.displayName}`);
-    } else {
-      console.log(`No intent matched.`);
-    }
+    // const result = responses[0].queryResult;
+    // console.log(`Query: ${result.queryText}`);
+    // console.log(`Response: ${result.fulfillmentText}`);
+    // if (result.intent) {
+    //   console.log(`Intent: ${result.intent.displayName}`);
+    // } else {
+    //   console.log(`No intent matched.`);
+    // }
 
-    res.send(result);
+    // res.send(result);
   });
 
   app.post('/api/df_event_query', async (req, res) => {
