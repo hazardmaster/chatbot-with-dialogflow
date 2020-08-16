b07a1046b231f901de0cabd269aa3886365cdae2
Oscar Hazard <oscarkipkoech@gmail.com>
2020-08-16 15:13:31 +0300

    further changes

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ require('./routes/dialogFlowRoutes')(app);
 //Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   //Set static folder
-  app.use(express.static('client/build'));
+  app.use('/', express.static(path.join(__dirname, '/client/build')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
